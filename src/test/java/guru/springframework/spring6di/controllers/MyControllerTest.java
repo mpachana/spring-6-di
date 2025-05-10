0a864d7a8545055344ed70a5378a794aa8d8522a
Michal Pachana <michal.pachana@gmail.com>
2025-05-10 16:27:28 +0200

    DI with spring (autowire on setter instead of property to avoid slower option reflection)

--- src/test/java/guru/springframework/spring6di/controllers/MyControllerTest.java
+++ src/test/java/guru/springframework/spring6di/controllers/MyControllerTest.java
@@ -2,6 +2,8 @@ package guru.springframework.spring6di.controllers;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class MyControllerTest {
 
     @Test
